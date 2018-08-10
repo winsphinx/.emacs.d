23ed1b3707b075a44968d77204f725145663bbbc
winsphinx <winsphinx@gmail.com>
2018-08-11 07:14:47 +0800

    no use line number

--- init.el
+++ init.el
@@ -419,9 +419,7 @@ It should only modify the values of Spacemacs settings."
    ;;                       text-mode
    ;;   :size-limit-kb 1000)
    ;; (default nil)
-   dotspacemacs-line-numbers '(:relative t
-                               :enabled-for-modes prog-mode
-                               :size-limit-kb 1000)
+   dotspacemacs-line-numbers nil
 
    ;; Code folding method. Possible values are `evil' and `origami'.
    ;; (default 'evil)
