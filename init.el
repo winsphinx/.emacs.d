03ae721abe4f34f9bf21d37f509c9ff39e4d16b7
winsphinx <winsphinx@gmail.com>
2018-09-13 21:54:23 +0800

    workaround for semantic slowdown elisp

--- init.el
+++ init.el
@@ -583,6 +583,15 @@ before packages are loaded."
       (org-todo (if (= n-not-done 0) "DONE" "TODO"))))
   (add-hook 'org-after-todo-statistics-hook 'org-summary-todo)
 
+  (defun et/semantic-remove-hooks ()
+    (remove-hook 'completion-at-point-functions
+                 'semantic-analyze-completion-at-point-function)
+    (remove-hook 'completion-at-point-functions
+                 'semantic-analyze-notc-completion-at-point-function)
+    (remove-hook 'completion-at-point-functions
+                 'semantic-analyze-nolongprefix-completion-at-point-function))
+  (add-hook 'semantic-mode-hook #'et/semantic-remove-hooks)
+
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
