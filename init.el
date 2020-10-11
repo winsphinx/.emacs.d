a744ca34fe6b45eb5829f5accfbc3f8a779e5fed
winsphinx <winsphinx@gmail.com>
2020-10-11 21:08:52 +0800

    modify settings less evil, more emacs, but still hjkl for motion

--- init.el
+++ init.el
@@ -261,8 +261,8 @@ It should only modify the values of Spacemacs settings."
    ;; (default 'vim)
    dotspacemacs-editing-style '(hybrid :variables
                                        hybrid-style-visual-feedback t
-                                       hybrid-style-enable-evilified-state t
-                                       hybrid-style-enable-hjkl-bindings nil
+                                       hybrid-style-enable-hjkl-bindings t
+                                       hybrid-style-enable-evilified-state nil
                                        hybrid-style-use-evil-search-module nil
                                        hybrid-style-default-state 'normal)
 
