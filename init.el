349d7907447ff175b16793974def3a12fa6ee439
winsphinx <winsphinx@gmail.com>
2020-11-25 09:18:38 +0800

    remove unicode-fonts layer, which causes startup slowly

--- init.el
+++ init.el
@@ -154,9 +154,6 @@ This function should only modify configuration layer settings."
                treemacs-use-follow-mode 'tag
                treemacs-use-git-mode 'deferred)
      (typography)
-     (unicode-fonts :variables
-                    unicode-fonts-enable-ligatures t
-                    unicode-fonts-ligature-modes '(prog-mode))
      (version-control)
      (yaml)
      )
