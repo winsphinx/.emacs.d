6925a0ca1cf89bc08561a5e02b9b6ea9cc1847d0
winsphinx <winsphinx@gmail.com>
2020-11-30 14:11:40 +0800

    modify an org-capture template

--- init.el
+++ init.el
@@ -679,7 +679,7 @@ before packages are loaded."
                    `("", (concat "* TODO " title), ":PROPERTIES:", (concat ":EXPORT_FILE_NAME: " fname), ":END:", "%?\n#+hugo: more\n")
                    "\n")))
     (setq org-capture-templates '(("t" "Todos" entry (file "~/.org/Tasks.org")
-                                   "* TODO %?\n\t%a\n\t%i\n")
+                                   "* TODO %?\n\tSCHEDULED: %(org-insert-time-stamp (org-read-date nil t \"+0d\"))\n\t%a\n\t%i\n")
                                   ("n" "Notes" entry (file+headline "~/.org/Notes.org" "NOTES")
                                    "* %?\n\t%i\n\t%a\n\t%U" :empty-lines 1)
                                   ("j" "Journal" entry (file+olp+datetree "~/.org/Journal.org")
