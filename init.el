9b889084a8033edc0c86e9b4cd8222c9379763d3
winsphinx <winsphinx@gmail.com>
2021-01-05 19:44:54 +0800

    modify org-capture-templates

--- init.el
+++ init.el
@@ -684,11 +684,11 @@ before packages are loaded."
                    `("", (concat "* TODO " title), ":PROPERTIES:", (concat ":EXPORT_FILE_NAME: " fname), ":END:", "%?\n#+hugo: more\n")
                    "\n")))
     (setq org-capture-templates '(("t" "Todos" entry (file "~/.org/Tasks.org")
-                                   "* TODO %?\n\tSCHEDULED: %(org-insert-time-stamp (org-read-date nil t \"+0d\"))\n\t%a\n\t%i\n")
+                                   "* TODO %?\n\tSCHEDULED: %(org-insert-time-stamp (org-read-date nil t \"+0d\"))\n\t%a\n\t%i")
                                   ("n" "Notes" entry (file+headline "~/.org/Notes.org" "NOTES")
                                    "* %?\n\t%i\n\t%a\n\t%U" :empty-lines 1)
                                   ("j" "Journal" entry (file+olp+datetree "~/.org/Journal.org")
-                                   "* %?\n\t%i\n\t%t" :clock-in t :clock-resume t)
+                                   "* %?\n\tSCHEDULED: %t\n\t%i" :clock-in t :clock-resume t)
                                   ("b" "Blogs" entry (file "~/.org/Blogs.org")
                                    (function org-hugo-new-subtree-post-capture-template) :empty-lines 1))))
 
