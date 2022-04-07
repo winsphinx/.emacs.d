285ae8f1d8d7e3aee66dfbf51eb6571622b4fbb0
winsphinx <winsphinx@gmail.com>
2022-04-07 09:44:51 +0800

    format code

--- init.el
+++ init.el
@@ -794,9 +794,7 @@ This function is called at the very end of Spacemacs initialization."
    ;; If you edit it by hand, you could mess it up, so be careful.
    ;; Your init file should contain only one such instance.
    ;; If there is more than one, they won't work right.
-   '(package-selected-packages
-     (quote
-      ())))
+   '(package-selected-packages 'nil))
   (custom-set-faces
    ;; custom-set-faces was added by Custom.
    ;; If you edit it by hand, you could mess it up, so be careful.
