90668c822534619efab6c25086cbe8feec004984
winsphinx <winsphinx@gmail.com>
2022-05-12 08:35:55 +0800

    use `org-roam-ui`

--- init.el
+++ init.el
@@ -115,7 +115,7 @@ This function should only modify configuration layer settings."
           org-enable-github-support t
           org-enable-hugo-support t
           org-enable-roam-protocol t
-          org-enable-roam-server t
+          org-enable-roam-ui t
           org-enable-roam-support t
           org-enable-transclusion-support t
           org-enable-valign t
