2445355f7c1f887401f73b0dc4f3b8a1b6be844c
winsphinx <winsphinx@gmail.com>
2022-05-30 22:17:45 +0800

    replace `rcirc` with `erc`
    This reverts commit d05984952e3ff3bf4306605a72030f1c9e0ca6a3.

--- init.el
+++ init.el
@@ -69,6 +69,7 @@ This function should only modify configuration layer settings."
            deft-directory "~/.org/notes/"
            deft-extensions '("org" "md" "txt"))
      (emacs-lisp)
+     (erc)
      (git :variables
           transient-display-buffer-action '(display-buffer-below-selected))
      (gnus)
@@ -161,10 +162,6 @@ This function should only modify configuration layer settings."
              ranger-show-preview t
              ranger-show-literal nil
              ranger-max-preview-size 10)
-     (rcirc :variables
-            rcirc-enable-styles t
-            rcirc-enable-emojify t
-            rcirc-enable-erc-image t)
      (restclient :variables
                  restclient-use-org t)
      (search-engine)
