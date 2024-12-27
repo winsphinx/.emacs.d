213ef42a65c951aac8cd6e6da43af7722a599f95
winsphinx <winsphinx@gmail.com>
2024-12-28 07:56:45 +0800

    change font size

--- init.el
+++ init.el
@@ -389,8 +389,8 @@ It should only modify the values of Spacemacs settings."
    ;; fixed-pitch faces. The `:size' can be specified as
    ;; a non-negative integer (pixel size), or a floating-point (point size).
    ;; Point size is recommended, because it's device independent. (default 10.0)
-   dotspacemacs-default-font '("iosevka"
-                               :size 12.0
+   dotspacemacs-default-font '("Iosevka"
+                               :size 14.0
                                :weight normal
                                :width normal)
 
