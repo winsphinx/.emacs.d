85dbdce91dcdcaf9ec907a99e43f036f7ddb8993
winsphinx <winsphinx@gmail.com>
2025-10-24 14:42:18 +0800

    disable undo-fu-session

--- init.el
+++ init.el
@@ -203,7 +203,7 @@ This function should only modify configuration layer settings."
    dotspacemacs-frozen-packages '()
 
    ;; A list of packages that will not be installed and loaded.
-   dotspacemacs-excluded-packages '()
+   dotspacemacs-excluded-packages '(undo-fu-session)
 
    ;; Defines the behaviour of Spacemacs when installing packages.
    ;; Possible values are `used-only', `used-but-keep-unused' and `all'.
